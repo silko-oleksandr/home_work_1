232e68ad704962013bb0506f18d6314baf246260
Александр Силко <mr.natov@gmail.com>
2026-05-27 19:34:11 +0300

    hw_12

--- home_work.py
+++ home_work.py
@@ -1,22 +1,20 @@
-#10.1
-def some_gen(begin, end, func):
-    current = begin
-    for _ in range(end):
-        yield current
-        current = func(current)
-#10.2
-def first_word(text):
-    for i, char in enumerate(text):
-        if char.isalpha() or char == "'":
-            start = i
-            break
-    for i in range(start, len(text)):
-        if not (text[i].isalpha() or text[i] == "'"):
-            return text[start:i]
-    return text[start:]
-#10.3
-def is_even(digit):
-    """
-    Перевірка чи є парним число
-    """
-    return digit % 2 == 0
+#12.1
+def prime_generator(end):
+    for num in range(2, end + 1):
+        for divisor in range(2, int(num ** 0.5) + 1):
+            if num % divisor == 0:
+                break
+        else:
+            yield num
+#12.2
+def generate_cube_numbers(end):
+    num = 2
+    while True:
+        cube = num ** 3
+        if cube > end:
+            return
+        yield cube
+        num += 1
+#12.3
+def is_even(number):
+    return (number & 1) == 0
